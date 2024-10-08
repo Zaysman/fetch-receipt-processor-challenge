e5c876572ac981e8929b2a4b664b99fc1e984707
Isaiah <i.x.smith137@gmail.com>
2024-10-08 15:27:06 -0400

    Finished the ReceiptService.

--- src/main/java/com/isaiah/receipt_processor_challenge/services/ReceiptService.java
+++ src/main/java/com/isaiah/receipt_processor_challenge/services/ReceiptService.java
@@ -2,12 +2,27 @@ package com.isaiah.receipt_processor_challenge.services;
 
 import org.springframework.stereotype.Service;
 
+import com.isaiah.receipt_processor_challenge.components.ScoreCalculator;
+import com.isaiah.receipt_processor_challenge.objects.ReceiptManager;
+import com.isaiah.receipt_processor_challenge.objects.Receipt;
+import com.isaiah.receipt_processor_challenge.objects.ScoreBreakdown;
+
+
 @Service
 public class ReceiptService {
 	
 	
+	//Creates a new receipt to be stored by the receipt manager. Returns the ID of the receipt that's generated
+	public String createReceipt(Receipt receipt) {	
+		ReceiptManager.saveReceipt(receipt); //Save the receipt. Receipt manager will handle generating the ID.
+		return receipt.getID(); //return the id
+	}
 	
 	
+	public int calculateScore(Receipt receipt) {
+		ScoreBreakdown scoreBreakdown = ScoreCalculator.calculateReceiptPoints(receipt);
+		return scoreBreakdown.getTotalPoints();
+	}
 	
 
 }
